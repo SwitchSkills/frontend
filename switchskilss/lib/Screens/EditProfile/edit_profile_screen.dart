ade33591db77c34d7e3d9f8f0c8f7ac866c458de
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-09-06 22:52:42 +0200

    save

--- switchskilss/lib/Screens/EditProfile/edit_profile_screen.dart
+++ switchskilss/lib/Screens/EditProfile/edit_profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import '../../components/background.dart';
 import '../../responsive.dart';
+import 'components/autodropdown.dart';
+
+final List<String> allSkills = ['Java', 'Python', 'Flutter', 'Dart', 'JavaScript', 'React', 'Angular', 'Vue'];
 
 class EditProfileScreen extends StatelessWidget {
   final String firstName;
@@ -31,8 +34,7 @@ class EditProfileScreen extends StatelessWidget {
             lastName: lastName,
             email: email,
             phoneNumber: phoneNumber,
-            ),
-
+          ),
         ),
       ),
     );
@@ -43,27 +45,44 @@ class DesktopEditProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
-      ],
+      children: [],
     );
   }
 }
 
-class MobileEditProfileScreen extends StatelessWidget {
-  final TextEditingController firstNameController;
-  final TextEditingController lastNameController;
-  final TextEditingController emailController;
-  final TextEditingController phoneNumberController;
+class MobileEditProfileScreen extends StatefulWidget {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
 
   MobileEditProfileScreen({
-    required String firstName,
-    required String lastName,
-    required String email,
-    required String phoneNumber,
-  })  : firstNameController = TextEditingController(text: firstName),
-        lastNameController = TextEditingController(text: lastName),
-        emailController = TextEditingController(text: email),
-        phoneNumberController = TextEditingController(text: phoneNumber);
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+  });
+
+  @override
+  _MobileEditProfileScreenState createState() => _MobileEditProfileScreenState();
+}
+
+class _MobileEditProfileScreenState extends State<MobileEditProfileScreen> {
+  late TextEditingController firstNameController;
+  late TextEditingController lastNameController;
+  late TextEditingController emailController;
+  late TextEditingController phoneNumberController;
+  List<String> selectedSkills = [];
+  final TextEditingController skillsController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    firstNameController = TextEditingController(text: widget.firstName);
+    lastNameController = TextEditingController(text: widget.lastName);
+    emailController = TextEditingController(text: widget.email);
+    phoneNumberController = TextEditingController(text: widget.phoneNumber);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -106,21 +125,45 @@ class MobileEditProfileScreen extends StatelessWidget {
               border: OutlineInputBorder(),
             ),
           ),
+          SizedBox(height: 12),
 
+          AutocompleteDropDown(
+            options: allSkills,
+            labelText: "Select Skills",
+            validator: (value) {
+                if (allSkills.contains(value ?? '')) {
+                    return null; 
+                } else {
+                    return 'Invalid Skill';
+                }
+            },
+
+            onItemSelected: (selected) {
+                // Optional callback to handle the selected item
+                print("Selected Skill: $selected");
+            },
+        ),
+        
 
           Spacer(),
           ElevatedButton(
             onPressed: () {
               // Handle save functionality here
-              // For example, you can print out the edited fields:
-              print('First Name: ${firstNameController.text}');
-              print('Last Name: ${lastNameController.text}');
-              print('Email: ${emailController.text}');
             },
             child: Text('Save'),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    phoneNumberController.dispose();
+    skillsController.dispose();
+    super.dispose();
   }
 }
