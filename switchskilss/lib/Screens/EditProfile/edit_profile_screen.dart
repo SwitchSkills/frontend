7d945c52e9071dc53d620081229fc479a2044083
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-09-07 20:14:43 +0200

    save edit profile

--- switchskilss/lib/Screens/EditProfile/edit_profile_screen.dart
+++ switchskilss/lib/Screens/EditProfile/edit_profile_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../components/background.dart';
 import '../../responsive.dart';
-import 'components/autodropdown.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
 
 final List<String> allSkills = ['Java', 'Python', 'Flutter', 'Dart', 'JavaScript', 'React', 'Angular', 'Vue'];
 
@@ -73,7 +74,12 @@ class _MobileEditProfileScreenState extends State<MobileEditProfileScreen> {
   late TextEditingController emailController;
   late TextEditingController phoneNumberController;
   List<String> selectedSkills = [];
+  List<String> selectedRegions = [];
   final TextEditingController skillsController = TextEditingController();
+  final TextEditingController regionsController = TextEditingController();
+  final List<String> allSkills = ['Java', 'Flutter', 'Python', 'C++', 'Dart', 'React', 'Node.js']; 
+  final List<String> allRegions = ['Limburg', 'Antwerpen', 'West-Vlaanderen'];
+
 
   @override
   void initState() {
@@ -125,33 +131,118 @@ class _MobileEditProfileScreenState extends State<MobileEditProfileScreen> {
               border: OutlineInputBorder(),
             ),
           ),
+          
           SizedBox(height: 12),
-
-          AutocompleteDropDown(
-            options: allSkills,
-            labelText: "Select Skills",
-            validator: (value) {
-                if (allSkills.contains(value ?? '')) {
-                    return null; 
-                } else {
-                    return 'Invalid Skill';
+          TypeAheadFormField<String>(
+            textFieldConfiguration: TextFieldConfiguration(
+              controller: skillsController,
+              decoration: InputDecoration(
+                labelText: 'Skills',
+                labelStyle: TextStyle(color: Colors.black),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            suggestionsCallback: (pattern) {
+              return allSkills.where((skill) => skill.toLowerCase().contains(pattern.toLowerCase())).toList();
+            },
+            itemBuilder: (context, String suggestion) {
+              return ListTile(
+                title: Text(suggestion),
+              );
+            },
+            onSuggestionSelected: (String suggestion) {
+              setState(() {
+                if (!selectedSkills.contains(suggestion)) {
+                  selectedSkills.add(suggestion);
                 }
+                skillsController.clear();
+              });
             },
+          ),
 
-            onItemSelected: (selected) {
-                // Optional callback to handle the selected item
-                print("Selected Skill: $selected");
+          SizedBox(height: 12),
+          Wrap(
+            spacing: 8,
+            children: selectedSkills.map((skill) {
+              return Chip(
+                label: Text(skill),
+                onDeleted: () {
+                  setState(() {
+                    selectedSkills.remove(skill);
+                  });
+                },
+                deleteIcon: Icon(Icons.close),
+              );
+            }).toList(),
+          ),
+
+          
+          TypeAheadFormField<String>(
+            textFieldConfiguration: TextFieldConfiguration(
+              controller: regionsController,
+              decoration: InputDecoration(
+                labelText: 'Regions',
+                labelStyle: TextStyle(color: Colors.black),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            suggestionsCallback: (pattern) {
+              return allRegions.where((region) => region.toLowerCase().contains(pattern.toLowerCase())).toList();
             },
-        ),
+            itemBuilder: (context, String suggestion) {
+              return ListTile(
+                title: Text(suggestion),
+              );
+            },
+            onSuggestionSelected: (String suggestion) {
+              setState(() {
+                if (!selectedRegions.contains(suggestion)) {
+                  selectedRegions.add(suggestion);
+                }
+                regionsController.clear();
+              });
+            },
+          ),
+
+          
+          Wrap(
+            spacing: 8,
+            children: selectedRegions.map((skill) {
+              return Chip(
+                label: Text(skill),
+                onDeleted: () {
+                  setState(() {
+                    selectedRegions.remove(skill);
+                  });
+                },
+                deleteIcon: Icon(Icons.close),
+              );
+            }).toList(),
+          ),
+          
+
+          
         
 
           Spacer(),
           ElevatedButton(
-            onPressed: () {
-              // Handle save functionality here
+            onPressed: () async {
+              try {
+                int statusCode = await updateUserInfo();
+                if (statusCode == 200) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('User info updated successfully!')),
+                  );
+                }
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Error updating user info: $e')),
+                );
+              }
             },
             child: Text('Save'),
           ),
+
         ],
       ),
     );
@@ -167,3 +258,5 @@ class _MobileEditProfileScreenState extends State<MobileEditProfileScreen> {
     super.dispose();
   }
 }
+
+
